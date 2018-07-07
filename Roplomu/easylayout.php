d18af3469656a4845fbcb4b12842bc3771d472ce
gimganghwi <gimganghwi@gmail.com>
2018-07-08 02:04:10 +0900

    roplo 레이아웃 해석기 테마 엔드삭스 제작, 데이터 분리
    샘플 json과 스타일시트 작성(엔드삭스)한다. 스위치 문으로 표현하던 데이터를 배열에 옮긴다.
    
    스타일시트는 자연스럽게 네 가지 부류로 나뉜다.
     - 강제로 적용할 덮어씌어질 수 없는 코드. 모든 스타일시트와 심지어 html까지 이 CSS와 강하게 결합해있어서 건들 수 없다.
     - 기본적으로 적용할 덮어 씌어질 수 있는 코드. 상식선에서 정상적인 사이트를 만들어준다. 이 코드들이 심하게 수정되면 사용자가 이용할 때 짜증날 것이다. 따라서 이 부분의 덮어쓰기는 테마를 통해서만 제공되고 폼으로 덮어씌우지 않는 것으로 하면 좋을 것이다.
     - 꽤 지엽적이지만 재사용 할 수 있는 테마코드. 재사용 할 수 있다는 말의 뜻은 유행처럼 대중적인 테마코드가 있기 때문이다. 아직은 레이아웃 요소를 표현하고 있는 부분이다. 주로 레이아웃 안의 레이아웃을 정한다.
     - 꾸미기용 코드. 배경색, 배경이미지, 버튼에 들어갈 그림 등을 정한다. 아마 이것들을 사용자가 세부조정 할 수 있게 기능을 제공할 것이다.

--- Roplomu/easylayout.php
+++ Roplomu/easylayout.php
@@ -1,9 +1,210 @@
 <?php 
 include('./common.php');
+// // roplo 스킨 질의
+// if ($roplo){
+// 	$roplo['style'] = sql_fetch("select json from {$r_table->style} where roplo_id = '{$roplo_id}' or roplo_id='default'")['json'];
+// }
 
-// roplo 스킨 질의
-if ($roplo){
-	$roplo['style'] = sql_fetch("select json from {$r_table->style} where roplo_id = '{$roplo_id}' or roplo_id='default'")['json'];
+$el=array();
+$layout = array();
+
+$layout['BODY'] = Array();
+	$layout['BODY']['tag'] = Array();
+		$layout['BODY']['tag']['start'] ='';
+		$layout['BODY']['tag']['end'] = '';
+
+$layout['ASIDE_OVERAY'] = Array();
+	$layout['ASIDE_OVERAY']['tag'] = Array();
+		$layout['ASIDE_OVERAY']['tag']['start'] ='<aside id="aside-overlay-$direction" style="--length:$length;" class="$direction overlay">';
+		$layout['ASIDE_OVERAY']['tag']['end'] = '</aside>';
+	$layout['ASIDE_OVERAY']['var'] = array();
+		$layout['ASIDE_OVERAY']['var']['length'] = '$length';
+		$layout['ASIDE_OVERAY']['var']['direction'] = '$direction';
+
+$layout['SECTION_FULL'] = Array();
+	$layout['SECTION_FULL']['tag'] = Array();
+		$layout['SECTION_FULL']['tag']['start'] ='<section id="section-full-$direction" style"--direction:$direction" class="$direction full $direction">';
+		$layout['SECTION_FULL']['tag']['end'] = '</section>';
+	$layout['SECTION_FULL']['var'] = array();
+		$layout['SECTION_FULL']['var']['direction'] = '$direction';
+
+$layout['ASIDE_PUSH'] = Array();
+	$layout['ASIDE_PUSH']['tag'] = Array();
+		$layout['ASIDE_PUSH']['tag']['start'] ='<aside id="aside-push-$direction "style="--length:$length" class="$direction push">';
+		$layout['ASIDE_PUSH']['tag']['end'] = '</aside>';
+	$layout['ASIDE_PUSH']['var'] = array();
+		$layout['ASIDE_PUSH']['var']['length'] = '$length';
+		$layout['ASIDE_PUSH']['var']['direction'] = '$direction';
+
+$layout['ASIDE_BUTTON'] = Array();
+	$layout['ASIDE_BUTTON']['tag'] = Array();
+		$layout['ASIDE_BUTTON']['tag']['start'] ='<a id="$anchor-button" class="aside-button" href="#$anchor">';
+		$layout['ASIDE_BUTTON']['tag']['end'] = '</a>';
+	$layout['ASIDE_BUTTON']['var'] = array();
+		$layout['ASIDE_BUTTON']['var']['anchor'] = '$anchor';
+
+$layout['WRAPPER'] = Array();
+	$layout['WRAPPER']['tag'] = Array();
+		$layout['WRAPPER']['tag']['start'] ='<div id="$id" class="$class wrapper">';
+		$layout['WRAPPER']['tag']['end'] = '</div>';
+	$layout['WRAPPER']['var'] = array();
+		$layout['WRAPPER']['var']['id'] = '$id';
+		$layout['WRAPPER']['var']['class'] = '$class';
+
+$content = array();
+$content['BOARD'] = Array();
+	$content['BOARD']['func'] = "BOARD";
+	$content['BOARD']['var'] = array();
+		$content['BOARD']['var']['id'] = '$id';
+		$content['BOARD']['var']['class'] = '$class';
+$content['LOGOSMALL'] = Array();
+	$content['LOGOSMALL']['func'] = "LOGOSMALL";
+	$content['LOGOSMALL']['var'] = array();
+		$content['LOGOSMALL']['var']['arg'] = '$arg';
+$content['LOGOBIG'] = Array();
+	$content['LOGOBIG']['func'] = "LOGOBIG";
+	$content['LOGOBIG']['var'] = array();
+		$content['LOGOBIG']['var']['arg'] = '$arg';
+$content['NAVIGATION'] = Array();
+	$content['NAVIGATION']['func'] = "NAVIGATION";
+	$content['NAVIGATION']['var'] = array();
+		$content['NAVIGATION']['var']['arg'] = '$arg';
+$content['TEXT'] = Array();
+	$content['TEXT']['func'] = "TEXT";
+	$content['TEXT']['var'] = array();
+		$content['TEXT']['var']['id'] = '$id';
+		$content['TEXT']['var']['class'] = '$class';
+
+function BOARD ( $arg ){ return  '<main></main>';}
+function LOGOSMALL ( $arg ){ global $roplo_id; return  "<img class='logo-small logo' src='".G5_DATA_URL."/roplomu/roplo/".$roplo_id."/logo-small.png'>";}
+function LOGOBIG ( $arg ){ global $roplo_id; return  "<img class='logo-big logo' src='".G5_DATA_URL."/roplomu/roplo/".$roplo_id."/logo-big.png'>";}
+function NAVIGATION ( $arg ){ return  '<ul><li>list1</li><li>list2</li><li>list3</li></ul>';}
+function TEXT ( $id, $class ){ return  "<span id='".$id."' class='".$class."'>ANDSOCKS - When it be finished?<br><i class='far fa-copyright'></i> cocopam 2018</span>";}
+
+$roplo['style']= '{
+"subtype" : "LAYOUT",
+"type": "BODY",
+"child":[
+	{ "subtype":"LAYOUT", "type":"ASIDE_OVERAY",
+	"var":{
+		"length":"72vw",
+		"direction":"left"	
+	},
+	"child":[
+
+		{ "subtype":"LAYOUT", "type":"ASIDE_BUTTON",
+		"var":{
+			"anchor":"none"
+		}},
+
+		{ "subtype":"CONTENT", "type":"LOGOBIG",
+		"var" : {
+			"arg":"arg"
+			}},
+
+		{"subtype":"LAYOUT", "type":"WRAPPER",
+		"var":{
+			"id":"paper_outter_navigation",
+			"class":"pink_round_paper"
+		},
+		"child":[
+
+			{"subtype":"CONTENT",
+			 "type":"NAVIGATION",
+			"var" : {
+				"arg":"arg"
+			}}
+
+		]},
+
+		{ "subtype":"CONTENT",
+		"type":"TEXT",
+		"var":{
+			"id":"content_inner_navigation",
+			"class":"light_transparent_text text"
+		}}
+
+	]},
+
+	{ "subtype":"LAYOUT",
+	"type":"SECTION_FULL",
+	"var":{
+		"direction":"column"
+		},
+	"child":[
+
+		{"subtype":"LAYOUT",
+		"type":"ASIDE_PUSH",
+		"length":"45px",
+		"var":{
+			"length":"50px",
+			"direction":"top"
+			},
+		"child":[
+
+			{ "subtype":"LAYOUT",
+			"type":"ASIDE_BUTTON",
+			"var":{
+				"anchor":"aside-overlay-left"
+				}},
+
+			{ "subtype":"CONTENT",
+			"type":"LOGOSMALL",
+			"var":{
+				"arg":"arg"
+			}},
+			
+			{ "subtype":"LAYOUT",
+			"type":"ASIDE_BUTTON" }
+		
+		]},
+
+		{"subtype":"LAYOUT",
+		"type":"SECTION_FULL",
+		"var":{
+			"direction":"row"
+			},
+		"child":[
+		
+			{ "subtype":"CONTENT",
+			"type":"BOARD",
+			"var":{
+				"arg":"arvg"
+			}}
+		]}
+	]}
+]}';
+
+
+function layoutElement($type, $var){
+	global $layout;
+
+	if ( isset( $layout[$type])) {
+		$el = $layout[$type];
+		foreach ($el["tag"] as $key => $tag ) {
+			$el["tag"][$key] = str_replace( $el['var'], $var, $tag );
+		}
+		return $el;
+	}
+	throw new Exception("Undefined layout element type - ".$type, 1);
+}
+
+function contentElement($type, $var){
+	global $content;
+	if ( isset( $content[$type])){
+		$el = $content[$type];
+		if ( isset($content[$type]['func']) ){
+		 	$func = $content[$type]['func'];
+		 	if ( $tag = call_user_func_array( $func, $var ) ){
+		 		return $tag;			
+		 	}
+		 	throw new Exception("Element type call strange function - ".$func, 1);
+		}
+		throw new Exception("Element type definition wrong. - ".$type, 1);
+	}
+	throw new Exception("Undefined content element type - ".$type, 1);
+
+	return array( );
 }
 
 ?>
@@ -15,112 +216,51 @@ if ($roplo){
 <meta name="HandheldFriendly" content="true">
 <meta name="format-detection" content="telephone=no">
 <link rel="stylesheet" type="text/css" href="easylayout.css">
+<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.1.0/css/all.css" integrity="sha384-lKuwvrZot6UHsBSfcMvOkWwlCMgc0TaWr+30HWe3a4ltaBwTZhyTEggF5tJv8tbt" crossorigin="anonymous">
+<link rel="stylesheet" href="http://doge.dothome.co.kr/js/font-awesome/css/font-awesome.min.css">
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
 
 <?php
-$codejson = $roplo['style'];
 
-$layout = json_decode($codejson, true);
+function style2html($style){
+	global $layout;
+	$html = "";
 
+	switch ($style['subtype']) {
+		case 'LAYOUT' :
+			$el =  layoutElement($style['type'], $style['var']);
+			$html .= $el['tag']['start'];
 
-function decodeLayout($layout){
-		$html='';
-		$el = element($layout);
-		
-		$html .=  $el['start'];
-		$html .= $el['content'];
-
-		if ( is_array($layout['child']) ){
-			// 자식 요소 추가
-			foreach ($layout['child'] as $child) {
-				$html .= decodeLayout($child);
+			if ( is_array($style['child']) ){
+				foreach ($style['child'] as $child) {
+					$html .= style2html( $child );
+				}
 			}
-		} else if ( $el['child']){
-			$html .= $el['child'];
-		}
 
-		$html .= $el['end'];
-		return $html;
-}
-
-
-function element($l){
-	switch ($l['subtype']) {
-		case 'BODY' : return array();
-		case 'LAYOUT': return layoutElement($l['type'], $l['length']);
-		case 'CONTENT' : return contentElement($l['type']);		
-		default: throw new Exception("정의되지 않은 서브타입".$l,  1);
+			$html .= $el['tag']['end'];
+			break;
+		case 'CONTENT' :
+			$el =  contentElement($style['type'], $style['var']);
+			$html .= $el;
+			break;
+		default:
+			throw new Exception("Undefined subtype - ".$style['subtype'],  1);
 	}
-}
 
-function contentElement($type){
-	$el=array();
-	switch ($type) {
-		case 'BOARD':
-			//$el['content'] = roplo_board();
-			$el['content'] ='<main></main>';
-		break;
-		case 'NAVIGATION':
-			$el['content'] ='<ul><li>list1</li><li>list2</li><li>list3</li></ul>';
-		break;
-		default: throw new Exception("정의되지 않은 타입".$l,  1);
-	}
-	return $el;
-}
 
-function layoutElement($type, $length){
-	/*
-	$sql = "select * in r_element where id='{$type}'";
-	$el =  $sql_fetch($sql);
-	for ($el as $tag) {
-		$tag = str_replace( '$length', $length ); 
-	}
-	return $el;
-	*/
-	$el=array();
-	switch ($type) {
-		case 'ASIDE_OVERAY_LEFT':
-			$el['start'] ='<aside id="aside-overlay-left" style="width:'.$length.'" class="left overlay">';
-			$el['end'] = '</aside>';
-			break;
-		case 'ASIDE_OVERAY_RIGHT':
-			$el['start'] ='<aside style="width:'.$length.'" class="right overlay">';
-			$el['end'] = '</aside>';
-			break;
-		case 'COLUMN_WRAPPER':
-			$el['start'] ='<section class="column-wrapper">';
-			$el['end'] = '</section>';
-			break;
-		case 'ASIDE_PUSH_TOP':
-			$el['start'] ='<aside style="height:'.$length.'" class="up">';
-			$el['end'] = '</aside>';
-			break;
-		case 'ROW_WRAPPER':
-			$el['start'] ='<section class="row-wrapper">';
-			$el['end'] = '</section>';
-			break;
-		case 'ASIDE_PUSH_LEFT':
-			$el['start'] ='<aside style="width:'.$length.'" class="left">';
-			$el['end'] = '</aside>';
-			break;
-		case 'ASIDE_OVERAY_LEFT_BUTTON':
-			$el['start'] ='<a id="aside-overlay-left-button-open" href="#aside-overlay-left">';
-			$el['child'] = '<span>열기</span>';
-			$el['end'] = '</a>';
-			break;
-		case 'ASIDE_CLOSE':
-			$el['start'] ='<a id="aside-close" href="#">';
-			$el['child'] = '<span>닫기</span>';
-			$el['end'] = '</a>';
-			break;
-		default: throw new Exception("정의되지 않은 타입".$l,  1);
-	}
-	return $el;
+	return $html;
 }
 
 ?>
 </head>
-<body>
-	<?php echo decodeLayout($layout) ?>
+<body class="roplo-viewport">
+	<?php 
+
+	$style=json_decode( $roplo['style'], true);
+	//var_dump($style);
+	echo style2html( $style );
+
+	?>
+
 </body>
 </html>
